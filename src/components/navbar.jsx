67d64e8d7000799937b8af76eff34276b4118e02
Savina Aliaj savina.aliaj@gmail.com <savina.aliaj@gmail.com>
2020-07-14 18:07:35 +0200

    add assets

---
+++ src/components/navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Navbar = () => {
+  return (
+    <div>
+      <h3>Playground.</h3>
+      <div>
+        <a href="#we-are">we are</a>
+        <a href="#we-do">we do</a>
+        <a href="#career">career</a>
+        <a href="#contact-us">contact us</a>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
--- src/index.jsx
+++ src/index.jsx
@@ -6,13 +6,12 @@ import '../assets/stylesheets/application.scss';
 const Hello = ({ name }) => {
   return (
     <div>
-      Hello,
-      {name}
+      lorem ipsum.
     </div>
   );
 };
 
 const root = document.getElementById('root');
 if (root) {
-  ReactDOM.render(<Hello name="World" />, root);
+  ReactDOM.render(<Hello />, root);
 }
