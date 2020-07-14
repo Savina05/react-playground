54986b20fd7120f39c2146a8dcfb3994e9a7e00c
Savina Aliaj savina.aliaj@gmail.com <savina.aliaj@gmail.com>
2020-07-14 18:18:52 +0200

    start navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,15 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component } from 'react';
 
 const Navbar = () => {
   return (
-    <div>
-      <h3>Playground.</h3>
+    <div className="navbar">
+      <h3 classNmame="navbar-h3">Playground.</h3>
       <div>
-        <a href="#we-are">we are</a>
-        <a href="#we-do">we do</a>
-        <a href="#career">career</a>
-        <a href="#contact-us">contact us</a>
+        <a href="#we-are" className="navbar_menu">we are</a>
+        <a href="#we-do" className="navbar_menu">we do</a>
+        <a href="#career" className="navbar_menu">career</a>
+        <a href="#contact-us" className="navbar_menu">contact us</a>
       </div>
     </div>
   );
--- src/index.jsx
+++ src/index.jsx
@@ -2,16 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import '../assets/stylesheets/application.scss';
+import App from './components/app';
 
-const Hello = ({ name }) => {
-  return (
-    <div>
-      lorem ipsum.
-    </div>
-  );
-};
 
 const root = document.getElementById('root');
 if (root) {
-  ReactDOM.render(<Hello />, root);
+  ReactDOM.render(<App />, root);
 }
