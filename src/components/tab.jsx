90fd80f1a158f8f512fdd74e8eb0cb57ba7d65f2
Savina Aliaj savina.aliaj@gmail.com <savina.aliaj@gmail.com>
2020-07-15 18:58:41 +0200

    complete API call in tab section

--- src/components/tab.jsx
+++ src/components/tab.jsx
@@ -1,35 +1,95 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronUp, faChevronDown } from '@fortawesome/free-solid-svg-icons';
 
-const Tabs = () => {
-  return (
-    <div id="tabs">
-      <div className="tabs">
-        <p className="tab_selection tab_selection_active">
-          Vestibulum at odio sit amet
-          <span className="tab_icon">
-            <FontAwesomeIcon icon={faChevronDown} />
-          </span>
-        </p>
-        <p className="tab_selection">
-          Sed vehicula neque
-          <span className="tab_icon">
-            <FontAwesomeIcon icon={faChevronUp} />
-          </span>
-        </p>
-        <p className="tab_selection">
-          Nulla id libero pretium
-          <span className="tab_icon">
-            <FontAwesomeIcon icon={faChevronUp} />
-          </span>
-        </p>
+
+class Tabs extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: ""
+    };
+  }
+
+  componentDidMount() {
+    fetch('./assets/ajax/tab1.json')
+      .then(response => response.json())
+      .then((data) => {
+        this.setState({
+          items: data.item.content
+        });
+      });
+  }
+
+  firstTab = () => {
+    fetch('./assets/ajax/tab1.json')
+      .then(response => response.json())
+      .then((data) => {
+        this.setState({
+          items: data.item.content
+        });
+      });
+    document.getElementById('first').classList.add('tab_selection_active');
+    document.getElementById('second').classList.remove('tab_selection_active');
+    document.getElementById('third').classList.remove('tab_selection_active');
+  }
+
+  secondTab = () => {
+    fetch('./assets/ajax/tab2.json')
+      .then(response => response.json())
+      .then((data) => {
+        this.setState({
+          items: data.item.content
+        });
+      });
+    document.getElementById('second').classList.add('tab_selection_active');
+    document.getElementById('first').classList.remove('tab_selection_active');
+    document.getElementById('third').classList.remove('tab_selection_active');
+  }
+
+  thirdTab = () => {
+    fetch('./assets/ajax/tab3.json')
+      .then(response => response.json())
+      .then((data) => {
+        this.setState({
+          items: data.item.content
+        });
+      });
+    document.getElementById('third').classList.add('tab_selection_active');
+    document.getElementById('first').classList.remove('tab_selection_active');
+    document.getElementById('second').classList.remove('tab_selection_active');
+  }
+
+
+  render() {
+    return (
+      <div id="tabs">
+        <div className="tabs">
+          <p className="tab_selection tab_selection_active" id="first" onClick={this.firstTab}>
+            Vestibulum at odio sit amet
+            <span className="tab_icon">
+              <FontAwesomeIcon icon={faChevronDown} />
+            </span>
+          </p>
+          <p className="tab_selection" id="second" onClick={this.secondTab}>
+            Sed vehicula neque
+            <span className="tab_icon">
+              <FontAwesomeIcon icon={faChevronUp} />
+            </span>
+          </p>
+          <p className="tab_selection" id="third" onClick={this.thirdTab}>
+            Nulla id libero pretium
+            <span className="tab_icon">
+              <FontAwesomeIcon icon={faChevronUp} />
+            </span>
+          </p>
+        </div>
+        <div className="tab_text">
+          <p>{this.state.items}</p>
+        </div>
       </div>
-      <div className="tab_text">
-        <p>Placeholder</p>
-      </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default Tabs;
